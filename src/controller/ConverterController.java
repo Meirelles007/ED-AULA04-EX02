139206bc24a5f6eb4d1c078391ddd8bda9cf6f8a
willi <willi@DESKTOP-689IO0P>
2021-09-16 17:43:49 -0300

    v2-o1

---
+++ src/controller/ConverterController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import br.edu.fateczl.hugobispo.pilhaint.Pilha;
+
+public class ConverterController {
+	Pilha p = new Pilha();
+	public String decTobin(int nDecimal){
+		String resultado ="";
+		int resto=0;
+		while(nDecimal!=0){
+			resto =  nDecimal%2;
+			nDecimal = nDecimal/2;
+			empilhar(resto);
+		}
+		resultado = desempilhar();	
+		return resultado;
+	}
+	
+	public String desempilhar() {
+		String resultado="";
+		while(p.isEmpty()==false) {
+			try {
+				int valor = p.pop();
+				resultado += Integer.toString(valor);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return resultado;
+	}
+
+	public void empilhar(int resto) {
+		p.push(resto);
+	}
+}
+	
+	
+
---
+++ src/view/Principal.java
@@ -0,0 +1,26 @@
+package view;
+import javax.swing.JOptionPane;
+import controller.ConverterController;
+
+public class Principal {
+	public static void main(String[] args) {
+		int nDecimal = 0;
+		String resultado="";
+		ConverterController Converter = new ConverterController();
+		boolean validador = true; 
+		while(validador==true) {
+			
+			try {
+				nDecimal = Integer.parseInt(JOptionPane.showInputDialog("Digite "
+						+ "um numero inteiro Decimal"));
+				resultado=Converter.decTobin(nDecimal);
+				JOptionPane.showMessageDialog(null,nDecimal+" Convertido em binario é " + resultado);
+				validador= false;
+			}
+			catch (Exception NumberFormatException){
+				JOptionPane.showMessageDialog(null, "Valor digitado invalido, digite novamente");
+				validador= true;
+			}
+		}
+	}	
+}
